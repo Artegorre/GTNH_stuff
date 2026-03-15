73fbdf4eaf26d10bcef085214e8267fbc378a6c7
Brieuc <brieuc.richoux@insa-rennes.fr>
2026-03-15 20:08:39 +0100

    added basic layout

--- energy_graph.lua
+++ energy_graph.lua
@@ -1,12 +1,33 @@
 local charts = require("charts")
 local component = require("component")
 
-local fuel_long = charts.Container{
-    x = 2,
-    y = 12,
+-- parameters
+local tank_face = 5
+
+local graph_width = 35
+local graph_height = 17
+local bar_width = 2 * graph_width + short_spacing
+local bar_height = 8
+local short_spacing = 2
+local long_spacing = 12
+
+-- fuel graphs
+
+
+local fuel_bar = charts.Container{
+    x = short_spacing,
+    y = short_spacing,
     bg = 0x808080,
-    width = 35,
-    height = 17
+    width = bar_width,
+    height = bar_height
+}
+
+local fuel_long = charts.Container{
+    x = short_spacing,
+    y = 2 * short_spacing + bar_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
 }
 local fuel_long_G = charts.Histogram {
     max = 41472000, -- change dinamically
@@ -17,9 +38,95 @@ local fuel_long_G = charts.Histogram {
 }
 fuel_long.payload = fuel_long_G
 
-local continue = 1
-while continue == 1 do
-    table.insert(fuel_long_G.values, component.transposer.getTankLevel(5))
+
+local fuel_long_delta = charts.Container{
+    x = 2 * short_spacing + graph_width,
+    y = 2 * short_spacing + bar_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+
+local fuel_short = charts.Container{
+    x = short_spacing,
+    y = 3 * short_spacing + bar_height + graph_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+
+local fuel_short_delta = charts.Container{
+    x = 2 * short_spacing + graph_width,
+    y = 3 * short_spacing + bar_height + graph_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+-- EU graphs
+
+local EU_bar = charts.Container{
+    x = 2 * short_spacing + 2 * graph_width + long_spacing,
+    y = short_spacing,
+    bg = 0x808080,
+    width = bar_width,
+    height = bar_height
+}
+
+local EU_long = charts.Container{
+    x = 2 * short_spacing + 2 * graph_width + long_spacing,
+    y = 2 * short_spacing + bar_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+
+local EU_long_delta = charts.Container{
+    x = 3 * short_spacing + 3 * graph_width + long_spacing,
+    y = 2 * short_spacing + bar_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+
+local EU_short = charts.Container{
+    x = 2 * short_spacing + 2 * graph_width + long_spacing,
+    y = 3 * short_spacing + bar_height + graph_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+
+local EU_short_delta = charts.Container{
+    x = 3 * short_spacing + 3 * graph_width + long_spacing,
+    y = 3 * short_spacing + bar_height + graph_height,
+    bg = 0x808080,
+    width = graph_width,
+    height = graph_height
+}
+
+-- draw 
+
+while 1 do
+    table.insert(fuel_long_G.values, component.transposer.getTankLevel(tank_face))
+    
+    -- stock containers and graphs in maps to do that more efficiently
+    fuel_bar:draw()
     fuel_long:draw()
+    fuel_long_delta:draw()
+    fuel_short:draw()
+    fuel_short_delta:draw()
+    EU_bar:draw()
+    EU_long:draw()
+    EU_long_delta:draw()
+    EU_short:draw()
+    EU_short_delta:draw()
+
+
     os.sleep(.05)
 end
